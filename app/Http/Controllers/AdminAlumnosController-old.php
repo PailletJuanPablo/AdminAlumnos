2301e821c5213ffc1fdf8d17fe04ddb57fac1f2f
PailletJuanPablo <pailletjuanpablo@gmail.com>
2019-04-06 17:01:59 -0300

    Tested

--- app/Http/Controllers/AdminAlumnosController-old.php
+++ app/Http/Controllers/AdminAlumnosController-old.php
@@ -184,8 +184,6 @@
 			if(Request::input('parent_id')){
 				$parent = Request::input('parent_id');
 				$this->index_statistic[] = ['label'=>'Total de Alumnos del curso','count'=>DB::table('alumnos')->where('actividad_id', $parent)->count(),'icon'=>'fa fa-check','color'=>'success'];
-			}else{
-				$this->index_statistic[] = ['label'=>'Total de Alumnos del curso','count'=>DB::table('alumnos')->where('actividad_id', 2)->count(),'icon'=>'fa fa-check','color'=>'success'];
 			}
 
 
